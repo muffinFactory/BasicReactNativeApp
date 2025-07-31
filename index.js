7dbd9934199cabb370fb3b19e7afc382c3666dd0
muffinFactory <muhharismaulana@gmail.com>
2025-07-31 10:48:52 +0700

    feat(dev): logbox ignore logs

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-/**
- * @format
- */
+import { AppRegistry, LogBox } from "react-native"
+import App from "./src/App"
+import { name as appName } from "./app.json"
 
-import {AppRegistry} from 'react-native';
-import App from './src/App';
-import {name as appName} from './app.json';
+if (process.env.NODE_ENV === "test") {
+  LogBox.ignoreAllLogs() // suppress all logs in tests
+} else {
+  LogBox.ignoreLogs(["Require cycle:"]) // suppress require-cycle warnings, it's fine
+}
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => App)
