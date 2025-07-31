012756fea88d167a0143cc2144deb1645064acf2
muffinFactory <muhharismaulana@gmail.com>
2025-07-31 10:22:51 +0700

    chore: workspace setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-native-a11y/ios',
+    'prettier',
+  ],
 };
